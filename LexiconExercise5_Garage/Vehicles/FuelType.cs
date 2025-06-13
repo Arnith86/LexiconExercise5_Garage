b74812b77df9a9524daa35133e72528d027b5818
a21jeaha <hisui86@gmail.com>
2025-06-13 17:11:33 +0200

    FuleType enum added

---
+++ LexiconExercise5_Garage/Vehicles/FuelType.cs
@@ -0,0 +1,12 @@
+﻿namespace LexiconExercise5_Garage.Vehicles;
+
+/// <summary>
+/// Enum representing different fuel types.
+/// </summary>
+public enum FuelType
+{
+	None,
+	Gasoline,
+	Diesel,
+	Electric
+}
