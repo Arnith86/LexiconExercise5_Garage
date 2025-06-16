cf78bd16cec65c35a8d384a69ac63c036afa13df
a21jeaha <hisui86@gmail.com>
2025-06-16 16:01:14 +0200

    Dynamic resizing of T[]  and AddVehicle() implemented + tests
    tests focus on a empty and full garage respectively and adding of vehicles.

--- LexiconExcercise5.Garage.TestProject/GaragesTests/GarageTests.cs
+++ LexiconExcercise5.Garage.TestProject/GaragesTests/GarageTests.cs
@@ -1,13 +1,20 @@
 ﻿using LexiconExcercise5.Garage.TestProject.Vehicles.Mocks;
 using LexiconExercise5_Garage.Garages;
+using LexiconExercise5_Garage.Vehicles;
 
 namespace LexiconExcercise5.Garage.TestProject.GaragesTests;
 
 /// <summary>
 /// Contains unit tests for the <see cref="Garage{T}"/> class
 /// </summary>
-public class GarageTests : IDisposable
+public class GarageTests 
 {
+	// VALID Vehicle base class attributes
+	private MockLicensePlateRegistry _c_MockLicensePlateRegistry = new MockLicensePlateRegistry();
+	private const string _c_LicensePlate = "BBK159";
+	private const VehicleColor _c_Color = VehicleColor.Blue;
+	private const uint _c_Wheel = 4;
+
 	// Valid garage sizes through constructor.
 	private const int _c_ArraySizeEdgeCaseMin1 = 1;
 	private const int _c_ArraySize2 = 2;
@@ -29,15 +36,17 @@ public class GarageTests : IDisposable
 	[InlineData(_c_ArraySizeEdgeCaseMin1)]
 	[InlineData(_c_ArraySize2)]
 	[InlineData(_c_ArraySizeBeforeLimit4)]
-	public void Vehicles_CapacityArraySize_SetViaConstructor_ValidValues_ShouldPass_ExpectedSize4(int size)
+	public void Vehicles_CapacityArraySize_SetViaConstructor_ValidValues_ShouldPass_Expected_UsedSpaces0_Capacity4(int size)
 	{
 		//Arrange & Act
-		int expected = 4;
-		Garage<MockVehicle> mockVehicles = new Garage<MockVehicle>(size);
+		int expectedUsedSpaces = 0;
+		int expectedCapacity = 4;
+		
+		Garage<MockVehicle> garage = new Garage<MockVehicle>(size);
 
 		//Assert
-		Assert.Equal(expected, mockVehicles.Capacity);
-		//Dispose
+		Assert.Equal(expectedCapacity, garage.Capacity);
+		Assert.Equal(expectedUsedSpaces, garage.UsedSpaces);
 	}
 
 
@@ -45,30 +54,34 @@ public class GarageTests : IDisposable
 	/// Verifies that a size just over a power-of-two boundary sets capacity to 8.
 	/// </summary>
 	[Fact]
-	public void Vehicles_CapacityArraySize_SetViaConstructor_ValidValues_ShouldPass_ExpectedSize8()
+	public void Vehicles_CapacityArraySize_SetViaConstructor_ValidValues_ShouldPass_UsedSpaces0_Capacity8()
 	{
 		//Arrange & Act
-		int expected = 8;
-		Garage<MockVehicle> mockVehicles = new Garage<MockVehicle>(_c_ArraySizeAfterLimit5);
+		int expectedUsedSpaces = 0;
+		int expectedCapacity = 8;
+
+		Garage<MockVehicle> garage = new Garage<MockVehicle>(_c_ArraySizeAfterLimit5);
 
 		//Assert
-		Assert.Equal(expected, mockVehicles.Capacity);
-		//Dispose
+		Assert.Equal(expectedCapacity, garage.Capacity);
+		Assert.Equal(expectedUsedSpaces, garage.UsedSpaces);
 	}
 
 	/// <summary>
 	/// Verifies that the maximum allowed size results in a capacity of 524288.
 	/// </summary>
 	[Fact]
-	public void Vehicles_CapacityArraySize_SetViaConstructor_ValidValues_ShouldPass_ExpectedSize524288()
+	public void Vehicles_CapacityArraySize_SetViaConstructor_ValidValues_ShouldPass_Expected_UsedSpaces0_Capacity524288()
 	{
 		//Arrange & Act
-		int expected = 524288;
-		Garage<MockVehicle> mockVehicles = new Garage<MockVehicle>(_c_ArraySizeEdgeCaseHighestValue);
+		int expectedUsedSpaces = 0;
+		int expectedCapacity = 524288;
+
+		Garage<MockVehicle> garage = new Garage<MockVehicle>(_c_ArraySizeEdgeCaseHighestValue);
 
 		//Assert
-		Assert.Equal(expected, mockVehicles.Capacity);
-		//Dispose
+		Assert.Equal(expectedCapacity, garage.Capacity);
+		Assert.Equal(expectedUsedSpaces, garage.UsedSpaces);
 	}
 
 
@@ -84,16 +97,72 @@ public class GarageTests : IDisposable
 		Assert.Throws<ArgumentOutOfRangeException>(() =>
 			new Garage<MockVehicle>(size)
 		);
-
-		//Dispose
 	}
 
-	/// <summary>
-	/// Clean-up method called after each test. Currently empty.
-	/// Use this to release shared resources if needed.
-	/// </summary>
-	public void Dispose()
+	[Fact]
+	public void AddVehicle_EmptyGarage_ShouldPass_Expected_UsedSpaces2_Capacity4()
 	{
-		// Cleanup logic here if needed
+		//Arrange & Act
+		int expectedUsedSpaces = 2;
+		int expectedCapacity = 4;
+
+		Garage<MockVehicle> garage = new Garage<MockVehicle>(_c_ArraySizeBeforeLimit4);
+
+		for (int i = 0; i < 2; i++)
+		{
+			garage.AddVehicle(
+				new MockVehicle(
+					licensePlateValidator => true,
+					_c_LicensePlate,
+					_c_Color,
+					_c_Wheel
+				)
+			);
+		}
+
+		//Assert
+		Assert.Equal(expectedCapacity, garage.Capacity);
+		Assert.Equal(expectedUsedSpaces, garage.UsedSpaces);
 	}
+
+	[Fact]
+	public void AddVehicle_FullGarage_ShouldThrowIndexOutOfRangeException()
+	{
+		//Arrange & Act
+	
+		Garage<MockVehicle> garage = new Garage<MockVehicle>(_c_ArraySizeEdgeCaseHighestValue);
+
+
+		for (int i = 0; i < 524288; i++)
+		{
+			garage.AddVehicle(
+				new MockVehicle(
+					LicensePlateIsUnique => true,
+					_c_LicensePlate,
+					_c_Color,
+					_c_Wheel
+				)
+			);
+		}
+		
+		//Assert
+		Assert.Throws<IndexOutOfRangeException>(() => 
+			garage.AddVehicle(
+				new MockVehicle(
+				LicensePlateIsUnique => true,
+				_c_LicensePlate,
+				_c_Color,
+				_c_Wheel)
+			)
+		);
+	}
+
+	///// <summary>
+	///// Clean-up method called after each test. Currently empty.
+	///// Use this to release shared resources if needed.
+	///// </summary>
+	//public void Dispose()
+	//{
+		
+	//}
 }
--- LexiconExercise5_Garage/Garages/Garage.cs
+++ LexiconExercise5_Garage/Garages/Garage.cs
@@ -1,40 +1,104 @@
 ﻿using LexiconExercise5_Garage.Vehicles;
 using System.Collections;
-
+using System.Threading.Channels;
 
 namespace LexiconExercise5_Garage.Garages;
 
+
+/// <summary>
+/// Represents a generic garage that can store a collection of vehicles up to a specified limit or upper limit of 524288.
+/// Manages capacity dynamically using an internal array with power-of-two sizing logic.
+/// </summary>
+/// <typeparam name="T">Types that inherit from VehicleBase.</typeparam>
 public class Garage<T> : IEnumerable<T> where T : VehicleBase
 {
-	// Upper limit for the garage size
-	private const int _c_ARRAY_SIZE_LIMIT = 524288;
-	private T[] _vehicles;
-	private int _capacity = 0;
+	// Upper and lower limits for the garage size
+	private const int _c_GARAGE_SIZE_UPPER_LIMIT = 524288;
+	private const int _c_GARAGE_SIZE_LOWER_LIMIT = 1;
 
+	private T[] _vehicles = new T[0];
+	private int _capacity = 0;
+	private int _garageVehicleLimit = 0;
+	private int _usedSpaces = 0;
+
+	/// <summary>
+	/// Gets the current capacity of the garage's internal array.
+	/// Expands the array size if the new capacity exceeds the current one.
+	/// </summary>
 	public int Capacity
-	{ 
+	{
 		get => _capacity;
-		private set 
+		private set
 		{
-			if (IsValidGarageSize(value))
+			if (IsWithinGarageMinMaxRange(value))
 			{
-				_capacity = FindArrayCapacitySize(value);
-				_vehicles = new T[_capacity];
+				int newCapacity = FindArrayCapacitySize(value);
+
+				if (IsCapacityToSmall(newCapacity)) ResizeVehicleArray(newCapacity);
 			}
 		}
 	}
 
 
-	// ToDo: Limit on size ? 
+	/// <summary>
+	/// Resizes the internal array to the new capacity while preserving existing vehicles.
+	/// </summary>
+	private void ResizeVehicleArray(int newCapacity)
+	{
+		T[] tempArray = new T[newCapacity];
+		Array.Copy(_vehicles, tempArray, _vehicles.Length);
+		_vehicles = tempArray;
+		_capacity = newCapacity;
+	}
+
+	/// <summary>
+	/// Gets the maximum number of vehicles that can be added to the garage.
+	/// </summary>
+	public int GarageVehicleLimit
+	{
+		get => _garageVehicleLimit;
+		private set
+		{
+			if (IsWithinGarageMinMaxRange(value))
+				_garageVehicleLimit = value;
+		}
+	}
+
+	/// <summary>
+	/// Gets the number of currently occupied spaces in the garage.
+	/// </summary>
+	public int UsedSpaces
+	{
+		get => _usedSpaces;
+		private set => _usedSpaces = value;
+	}
+
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="Garage{T}"/> class with a specified size limit.
+	/// </summary>
+	/// <param name="size">Initial size limit for the garage.</param>
 	public Garage(int size)
 	{
 		Capacity = size;
+		GarageVehicleLimit = size;
 	}
 
+
+	private bool IsCapacityToSmall(int newCapacity)
+	{
+		return _capacity < newCapacity;
+	}
+
+	/// <summary>
+	/// Calculates the array size as a power of two that is equal to or greater than the given size.
+	/// Minimum capacity is 4.
+	/// </summary>
 	private int FindArrayCapacitySize(int size)
 	{
 		int result = 0;
 
+		// Smallest sized array is 4
 		if (size > 0 && size < 5)
 		{
 			result = 4;
@@ -47,7 +111,7 @@ public class Garage<T> : IEnumerable<T> where T : VehicleBase
 			for (int i = 2; i < 20; i++)
 			{
 				ArraySizePowerOfTwoUpperLimit = (int)Math.Pow(2, i);
-				
+
 				if (size <= ArraySizePowerOfTwoUpperLimit)
 				{
 					result = ArraySizePowerOfTwoUpperLimit;
@@ -59,24 +123,49 @@ public class Garage<T> : IEnumerable<T> where T : VehicleBase
 		return result;
 	}
 
-	private bool IsValidGarageSize(int size)
+	private bool IsWithinGarageMinMaxRange(int size)
 	{
 		// ToDo: Catch and handle this exception.
-		if (size < 1 || size > _c_ARRAY_SIZE_LIMIT)
+		if (size < _c_GARAGE_SIZE_LOWER_LIMIT || size > _c_GARAGE_SIZE_UPPER_LIMIT)
 			throw new ArgumentOutOfRangeException(nameof(size), "Garage size must be within the range of 1 - 524288.");
-		
+
 		return true;
 	}
 
+	/// <summary>
+	/// Adds a vehicle to the garage.
+	/// Automatically resizes the internal array if necessary.
+	/// </summary>
+	/// <param name="vehicle">The vehicle to add.</param>
+	/// <returns>True if the vehicle was successfully added.</returns>
+	/// <exception cref="IndexOutOfRangeException">Thrown if garage is full.</exception>
+	public bool AddVehicle(T vehicle)
+	{
+		// ToDo: catch and handle this exception.
+		if (UsedSpaces == _c_GARAGE_SIZE_UPPER_LIMIT || UsedSpaces == GarageVehicleLimit)
+			throw new IndexOutOfRangeException(nameof(vehicle));
+
+		int nextIndex = UsedSpaces;
+		
+		Capacity = nextIndex + 1;
+		_vehicles[nextIndex] = vehicle;
+		UsedSpaces++;
+
+		return true;
+	}
+
+	/// <summary>
+	/// Returns an enumerator that iterates through the garage's stored vehicles.
+	/// </summary>
 	public IEnumerator<T> GetEnumerator()
 	{
 		// return a single value, then yield until next operation 
 		foreach (var vehicle in _vehicles)
 		{
-			if ( vehicle != null )
-				yield return vehicle;	
+			if (vehicle != null)
+				yield return vehicle;
 		}
-		
+
 		//return ((IEnumerable<T>)_vehicles).GetEnumerator();
 	}
 
