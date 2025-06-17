4341f269a2decd6cfb35d0a4cd424c722739475e
a21jeaha <hisui86@gmail.com>
2025-06-17 18:37:17 +0200

    Implemented a I and ConsoleWritePrint, abstract away console input output.

---
+++ LexiconExercise5_Garage/ConsoleRelated/CWritePrint/ConsoleWritePrint.cs
@@ -0,0 +1,31 @@
+﻿namespace LexiconExercise5_Garage.ConsoleRelated.CWritePrint
+{
+	/// <summary>
+	/// A class that abstracts console input and output operations.
+	/// </summary>
+	public class ConsoleWritePrint : IConsoleWritePrint
+	{
+		/// <inheritdoc/>
+		public void WriteLine(string message) => Console.WriteLine(message);
+
+		/// <inheritdoc/>
+		public void Write(string message) => Console.Write(message);
+
+
+		/// <inheritdoc/>
+		public string ReadLine() => Console.ReadLine() ?? string.Empty;
+
+		/// <inheritdoc/>
+		public void ForegroundColor(ConsoleColor color) =>
+			Console.ForegroundColor = color;
+
+		/// <inheritdoc/>
+		public void ResetConsoleColor() => Console.ResetColor();
+
+		/// <inheritdoc/>
+		public void Clear() => Console.Clear();
+
+		/// <inheritdoc/>
+		public void ReadKey() => Console.ReadKey();
+	}
+}
