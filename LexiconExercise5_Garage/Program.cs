9770f34b58aa1b6c9818ac5ebb9823489513a20e
a21jeaha <hisui86@gmail.com>
2025-06-17 15:54:09 +0200

    removed completed todo comments

--- LexiconExercise5_Garage/Program.cs
+++ LexiconExercise5_Garage/Program.cs
@@ -8,10 +8,7 @@ internal class Program
 {
     static void Main(string[] args)
     {
-		// ToDo: implement a garage<T> with a "where" with applicable constraints. It should implement/inherit from IEnumirable<T>. Garage capacity should be set in the constructor.
 		
-		// ToDo: The collection in Garage must be private and use an underlying array (think List<T>). 		
-	
 		// ToDo: implement a GarageHandler. (controller/ViewModel)
 
 		
