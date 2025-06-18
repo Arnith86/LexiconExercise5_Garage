2c6df468e41d6629c2db244ecbe8f951486ba9de
a21jeaha <hisui86@gmail.com>
2025-06-18 08:35:34 +0200

    method GarageCreation implemented with tests

---
+++ LexiconExcercise5.Garage.TestProject/GarageHandlersTests/GarageHandlerTests.cs
@@ -0,0 +1,78 @@
+﻿using LexiconExercise5_Garage.Garages;
+using LexiconExercise5_Garage.GaragesHandler;
+using LexiconExercise5_Garage.Vehicles;
+using LexiconExercise5_GarageAssignment.ConsoleRelated;
+using Moq;
+using System;
+
+namespace LexiconExcercise5.Garage.TestProject.GarageHandlersTests;
+
+
+public class GarageHandlerTests
+{
+	// Valid garage sizes through constructor.
+	private const int _c_ArraySizeEdgeCaseMin1 = 1;
+	private const int _c_ArraySize2 = 2;
+	private const int _c_ArraySizeBeforeLimit4 = 4;
+	private const int _c_ArraySizeAfterLimit5 = 5;
+	private const int _c_ArraySizeEdgeCaseHighestValue = 524288;
+
+	// Expected UsedSpaces values 
+	private const int _c_ExpectedUsedSpaces4 = 4;
+
+	// Invalid garage sizes.
+	private const int _c_ArraySizeNegative = -1;
+	private const int _c_ArraySizeOverUpperLimit524289 = 524289;
+
+
+	[Theory]
+	[InlineData(_c_ArraySizeEdgeCaseMin1)]
+	[InlineData(_c_ArraySizeBeforeLimit4)]
+	public void CreateGarage_ValidUserInput_ShouldPass(int size)
+	{
+		// Arrange
+		Mock<IConsoleUI> mockConsoleUI = new();
+		mockConsoleUI.Setup(cUI => cUI.GetGarageSize()).Returns(size);
+		GarageHandler garageHandler = new GarageHandler(mockConsoleUI.Object);
+
+		// Act
+		garageHandler.GarageCreation();
+
+		// Assert
+		mockConsoleUI.Verify(ui => ui.GarageCreated(size), Times.Once);
+	}
+
+	[Theory]
+	[InlineData(_c_ArraySizeNegative, _c_ArraySizeBeforeLimit4)]
+	[InlineData(_c_ArraySizeOverUpperLimit524289, _c_ArraySizeBeforeLimit4)]
+	public void CreateGarage_VerifyGarageCreationLoopFunctionsAsIntended_ShouldPass(int invalidSize, int validSize)
+	{
+		// Arrange
+		Mock<IConsoleUI> mockConsoleUI = new();
+		mockConsoleUI.SetupSequence(cUI => cUI.GetGarageSize())
+			.Returns(invalidSize)
+			.Returns(validSize); // Simulates valid input to end loop.
+
+		GarageHandler garageHandler = new GarageHandler(mockConsoleUI.Object);
+
+		// Act
+		garageHandler.GarageCreation();
+
+		// Assert
+		mockConsoleUI.Verify(cUI => cUI.ShowError(It.IsAny<string>()), Times.Once); // Exception was registered, message sent ConsoleUI.
+		mockConsoleUI.Verify(cUI => cUI.GarageCreated(validSize), Times.Once);	// Valid input registered
+	}
+
+
+	///// <summary>
+	///// Create a initialized <see cref="GarageHandler"/>.
+	///// </summary>
+	///// <returns>A initialized <see cref="GarageHandler"/>.</returns>
+	//public GarageHandler GarageHandlerCreator()
+	//{
+	//	ConsoleWritePrint consoleWritePrint = new();
+	//	ConsoleUI consoleUI = new(consoleWritePrint);
+
+	//	return new GarageHandler(consoleWritePrint, consoleUI);
+	//}
+}
---
+++ LexiconExercise5_Garage/GaragesHandler/GarageHandler.cs
@@ -0,0 +1,48 @@
+﻿
+
+using LexiconExercise5_Garage.Garages;
+using LexiconExercise5_Garage.Vehicles;
+using LexiconExercise5_GarageAssignment.ConsoleRelated;
+using System.Linq.Expressions;
+
+namespace LexiconExercise5_Garage.GaragesHandler;
+
+public class GarageHandler
+{
+	private readonly IConsoleUI _consoleUI;
+	private IGarage<VehicleBase> _garage;
+
+	public GarageHandler(IConsoleUI consoleUI)
+	{
+		_consoleUI = consoleUI;
+	}
+
+	/// <summary>
+	/// Initiates <see cref="Garages"/> creation.
+	/// </summary>
+	public void GarageCreation()
+	{
+		bool isValid = false;
+		int size = 0;
+
+		do
+		{
+			size = _consoleUI.GetGarageSize();
+
+			try
+			{
+				_garage = new Garage<VehicleBase>(size);
+				isValid = true;
+			}
+			catch (ArgumentOutOfRangeException e)
+			{
+				_consoleUI.ShowError(message: e.Message);
+			}
+
+		} while (!isValid);
+		
+
+		_consoleUI.GarageCreated(size);
+
+	}
+}
