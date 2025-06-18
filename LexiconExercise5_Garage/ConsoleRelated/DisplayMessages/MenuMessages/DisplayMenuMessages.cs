9098f7127c194a7e65508681201651681cf111bc
a21jeaha <hisui86@gmail.com>
2025-06-18 11:14:54 +0200

    added DisplayMenuMessages, used for formaring menu in console

---
+++ LexiconExercise5_Garage/ConsoleRelated/DisplayMessages/MenuMessages/DisplayMenuMessages.cs
@@ -0,0 +1,26 @@
+﻿using LexiconExercise5_Garage.ConsoleRelated.CWritePrint;
+
+namespace LexiconExercise5_Garage.ConsoleRelated.DisplayMessages.MenuMessages;
+
+/// <summary>
+/// Provides a console-based implementation for displaying menu messages with colored formatting.
+/// </summary>
+public class DisplayMenuMessages : IDisplayMenuMessages
+{
+	private readonly IConsoleWritePrint _consoleWP;
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="DisplayMenuMessages"/> class.
+	/// </summary>
+	/// <param name="consoleWritePrint">An implementation of <see cref="IConsoleWritePrint"/> used to handle console output operations.</param>
+	public DisplayMenuMessages(IConsoleWritePrint consoleWritePrint) =>
+		_consoleWP = consoleWritePrint;
+
+	/// <inheritdoc/>
+	public void DisplayMenu(string menu)
+	{
+		_consoleWP.ForegroundColor(ConsoleColor.Yellow);
+		_consoleWP.WriteLine(menu);
+		_consoleWP.ResetConsoleColor();
+	}
+}
---
+++ LexiconExercise5_Garage/ConsoleRelated/DisplayMessages/MenuMessages/IDisplayMenuMessages.cs
@@ -0,0 +1,17 @@
+﻿/// <summary>
+/// Contains the interface definition for displaying menu-related messages in the console UI.
+/// </summary>
+namespace LexiconExercise5_Garage.ConsoleRelated.DisplayMessages.MenuMessages
+{
+	/// <summary>
+	/// Defines a contract for displaying menu messages in the console application.
+	/// </summary>
+	public interface IDisplayMenuMessages
+	{
+		/// <summary>
+		/// Displays the specified menu string to the user.
+		/// </summary>
+		/// <param name="menu">A formatted string representing the menu to be displayed.</param>
+		void DisplayMenu(string menu);
+	}
+}
