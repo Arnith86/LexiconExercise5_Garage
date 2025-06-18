f32c52725d0920441f077ab63de8bd54338aa600
a21jeaha <hisui86@gmail.com>
2025-06-18 12:34:01 +0200

    added DI with enough added to start the main menu

--- LexiconExercise5_Garage/Program.cs
+++ LexiconExercise5_Garage/Program.cs
@@ -1,4 +1,11 @@
 ﻿using System.Runtime.CompilerServices;
+using LexiconExercise5_Garage.ConsoleRelated.CWritePrint;
+using LexiconExercise5_Garage.ConsoleRelated.DisplayMessages.ErrorMessages;
+using LexiconExercise5_Garage.ConsoleRelated.DisplayMessages.MenuMessages;
+using LexiconExercise5_Garage.GaragesHandler;
+using LexiconExercise5_GarageAssignment.ConsoleRelated;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 [assembly: InternalsVisibleTo("LexiconExercise5_Garage.TestProject")]
 
@@ -8,10 +15,22 @@ internal class Program
 {
     static void Main(string[] args)
     {
-		
-		// ToDo: implement a GarageHandler. (controller/ViewModel)
 
-		
+		var host = Host.CreateDefaultBuilder(args)
+			.ConfigureServices(services =>
+			{
+				services.AddSingleton<IConsoleUI, ConsoleUI>();
+				services.AddSingleton<IConsoleWritePrint, ConsoleWritePrint>();
+				services.AddSingleton<IDisplayErrorMessages, DisplayErrorMessages>();
+				services.AddSingleton<IDisplayMenuMessages, DisplayMenuMessages>();
+
+				services.AddSingleton<GarageHandler>();
+				
+			})
+			.UseConsoleLifetime()
+			.Build();
+
+		host.Services.GetRequiredService<GarageHandler>().MainMenuSelection();
 	
     }
 }
