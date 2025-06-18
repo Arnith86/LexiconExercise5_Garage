3e436eb9f7f17e5f764b2bf8aa717f8dfe1fbd06
a21jeaha <hisui86@gmail.com>
2025-06-18 12:35:45 +0200

    Create IConsoleUI.cs

---
+++ LexiconExercise5_Garage/ConsoleRelated/UI/IConsoleUI.cs
@@ -0,0 +1,10 @@
+﻿namespace LexiconExercise5_GarageAssignment.ConsoleRelated
+{
+	public interface IConsoleUI
+	{
+		int GetGarageSize();
+		void GarageCreated(int garageSize);
+		void ShowError(string message);
+		int RegisterMainMenuSelection();
+	}
+}
