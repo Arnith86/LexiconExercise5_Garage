cec640d10c113c21308da2672717eed172716a5d
a21jeaha <hisui86@gmail.com>
2025-06-18 13:14:08 +0200

    implemented I & DisplayFeedBackMessage
    formats feedback messages

---
+++ LexiconExercise5_Garage/ConsoleRelated/DisplayMessages/FeedbackMessage/DisplayFeedBackMessage.cs
@@ -0,0 +1,28 @@
+﻿using LexiconExercise5_Garage.ConsoleRelated.CWritePrint;
+
+namespace LexiconExercise5_Garage.ConsoleRelated.DisplayMessages.FeedbackMessage;
+
+/// <summary>
+/// Provides functionality to display feedback messages using a colored console output.
+/// </summary>
+public class DisplayFeedBackMessage : IDisplayFeedbackMessage
+{
+	private readonly IConsoleWritePrint _consoleWP;
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="DisplayFeedBackMessage"/> class.
+	/// </summary>
+	/// <param name="consoleWritePrint">An implementation of <see cref="IConsoleWritePrint"/> to handle console output.</param>
+	public DisplayFeedBackMessage(IConsoleWritePrint consoleWritePrint)
+	{
+		_consoleWP = consoleWritePrint;
+	}
+
+	/// <inheritdoc/>
+	void IDisplayFeedbackMessage.DisplayFeedBackMessage(string message)
+	{
+		_consoleWP.ForegroundColor(ConsoleColor.DarkBlue);
+		_consoleWP.WriteLine(message);
+		_consoleWP.ResetConsoleColor();
+	}
+}
---
+++ LexiconExercise5_Garage/ConsoleRelated/DisplayMessages/FeedbackMessage/IDisplayFeedbackMessage.cs
@@ -0,0 +1,13 @@
+﻿namespace LexiconExercise5_Garage.ConsoleRelated.DisplayMessages.FeedbackMessage;
+
+/// <summary>
+/// Defines a contract for displaying feedback messages to the user.
+/// </summary>
+public interface IDisplayFeedbackMessage
+{
+	/// <summary>
+	/// Displays a feedback message to the console or user interface.
+	/// </summary>
+	/// <param name="message">The message to be displayed.</param>
+	void DisplayFeedBackMessage(string message);
+}
